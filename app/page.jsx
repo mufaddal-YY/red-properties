f7213f209c97a21f88c8c7647f40a677ea5d01bb
Mufaddal Aurangabadi <mufaddalaurangabadi@gmail.com>
2024-08-05 11:59:03 +0530

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1 className="text-4xl font-bold text-slate-800">Next-Js-Template</h1>
+    </div>
+  );
+}
